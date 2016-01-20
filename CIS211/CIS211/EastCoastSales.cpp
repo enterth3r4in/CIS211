6983c53c4344e4773949ed4b399c9779ff93a02b
enterth3r4in <believe.the.rain@gmail.com>
2016-01-20 12:03:08 -0500

    Homework

---
+++ CIS211/CIS211/EastCoastSales.cpp
@@ -0,0 +1,15 @@
+//This program predicts how much the East Coast Division will generate of total sales.
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	float eastCoastPercentage = 0.62;
+	float companySales = 4600000.0;
+	float eastCoastSales;
+
+
+
+	return 0;
+}
